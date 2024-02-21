eae089a34f2f1c77889f54764c38d55184ed97a5
FireMatrix <tim.schroeder02@gmx.de>
2024-02-21 20:32:26 +0100

    submenus remember selected line of higher order menu

--- matrixscripts/core/term_core.lua
+++ matrixscripts/core/term_core.lua
@@ -18,7 +18,7 @@ M.border_up = window.create(term.current(), 1, 1, M.term_xmax, 1)
 M.border_down = window.create(term.current(), 1, M.term_ymax, M.term_xmax, 1)
 M.mainArea = window.create(term.current(), 3, 2, M.term_xmax-4, M.term_ymax-2)
 M.bottomArea = window.create(term.current(), 3, M.term_ymax-1, M.term_xmax-4, 1)
---TODO doc
+
 --[[
     LABEL: text, not selectable
     MENU: Opens a submenu
@@ -26,8 +26,6 @@ M.bottomArea = window.create(term.current(), 3, M.term_ymax-1, M.term_xmax-4, 1)
 ]]
 M.options = {}
 M.exitName = "exit terminal"
---TODO doc
-M.selected_line = 0
 M.numkeys = {
     zero=0,
     one=1,
@@ -41,7 +39,7 @@ M.numkeys = {
     nine=9
 }
 
-function M:drawTerminal(text, auxText)
+function M:drawTerminal(text, auxText, selected_line)
     term.clear()
     self.mainArea.clear()
     self.bottomArea.clear()
@@ -64,7 +62,7 @@ function M:drawTerminal(text, auxText)
 
     for index, value in ipairs(text) do
         self.mainArea.setCursorPos(1,index)
-        if self.selected_line == index then
+        if selected_line == index then
             self.mainArea.setTextColor(colors.white)
         else
             self.mainArea.setTextColor(colors.lime)
@@ -76,26 +74,25 @@ function M:drawTerminal(text, auxText)
 end
 
 --option: name, valueType, default
-function M:displayOptions(options, auxText)
+function M:displayOptions(options, auxText, selected_line)
     local text = {}
     for i, current_option in ipairs(options) do
         text[i] = current_option["name"] .. (options[i]["type"] == "var" and " | " .. tostring(options[i]["value"]["value"]) or "")
     end
 
-    self:drawTerminal(text, auxText)
+    self:drawTerminal(text, auxText, selected_line)
 end
 
-function M:selectFirstEntry(options)
+function M:getFirstSelectableIndex(options)
     for i = 1, #options, 1 do
         if options[i]["type"] ~= "label" then
-            self.selected_line = i
-            break
+            return i
         end
     end
+    return nil
 end
 
 function M:driveVarChange(varArgs)
-    self.selected_line = 1
     local exited = false
     local argc = 1
         local options = {{name=varArgs["name"], type="var", value={value=varArgs["value"]["value"]}}}
@@ -109,7 +106,7 @@ function M:driveVarChange(varArgs)
         end
 
     while not exited do
-        self:displayOptions(options, "  Enter to Select, Delete to Exit")
+        self:displayOptions(options, "   Enter to Save, Delete to Exit", 1)
         local _, key = os.pullEvent("key")
         key = keys.getName(key);
         (({
@@ -139,9 +136,9 @@ end
 
 function M:driveMenu(options)
     local exited = false
-    self:selectFirstEntry(options)
+    local selected_line = self:getFirstSelectableIndex(options)
     while not exited do
-        self:displayOptions(options, "Arrow Keys to Move, Enter to Select")
+        self:displayOptions(options, "Arrow Keys to Move, Enter to Select", selected_line)
         --look for key presses
         local _, key = os.pullEvent("key")
         key = keys.getName(key);
@@ -153,31 +150,29 @@ function M:driveMenu(options)
                         exited = true
                     end,
                     menu = function ()
-                        local nextOptions = options[self.selected_line]["value"]
+                        local nextOptions = options[selected_line]["value"]
                         if nextOptions[#nextOptions]["type"] ~= "exit" then
                             nextOptions[#nextOptions+1] = {name="back", type="exit"}
                         end
                         self:driveMenu(nextOptions)
                     end,
                     var = function ()
-                        self:driveVarChange(options[self.selected_line])
+                        self:driveVarChange(options[selected_line])
                     end
-                })[options[self.selected_line]["type"]] or function () end)()
-                
-                self:selectFirstEntry(options)
+                })[options[selected_line]["type"]] or function () end)()
             end,
             down = function ()
-                for i = self.selected_line+1, #options, 1 do
+                for i = selected_line+1, #options, 1 do
                     if options[i]["type"] ~= "label" then
-                        self.selected_line = i
+                        selected_line = i
                         return
                     end
                 end
             end,
             up = function ()
-                for i = self.selected_line-1, 1, -1 do
+                for i = selected_line-1, 1, -1 do
                     if options[i]["type"] ~= "label" then
-                        self.selected_line = i
+                        selected_line = i
                         return
                     end
                 end
